f1a7cfa92c0c782b501824058b422d94535d08a9
WarBoar86 <paulo.carucio@gmail.com>
2022-04-13 00:08:40 -0300

    SideBar and Content components implemented

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,38 @@
-export function Content() {
+import { MovieCard } from "./MovieCard";
+import { GenreResponseProps } from "./SideBar";
+
+export interface MovieProps {
+  imdbID: string;
+  Title: string;
+  Poster: string;
+  Ratings: Array<{
+    Source: string;
+    Value: string;
+  }>;
+  Runtime: string;
+}
+
+interface ContentParams{
+  movies: MovieProps[],
+  selectedGenre: GenreResponseProps
+} 
+
+export function Content( {selectedGenre, movies}: ContentParams) {
   // Complete aqui
+
+  return(
+    <div className="container">
+      <header>
+        <span className="category">Categoria:<span> {selectedGenre.title}</span></span>
+      </header>
+
+      <main>
+        <div className="movies-list">
+          {movies.map(movie => (
+            <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+          ))}
+        </div>
+      </main>
+    </div>
+  );
 }
